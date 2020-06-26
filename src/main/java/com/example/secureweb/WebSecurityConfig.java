ba38423ec2bbc7749b82a0b804e3723be36cc393
vtolas <vasileios.tolas@mni.thm.de>
2020-06-26 18:48:50 +0200

    fix login

--- src/main/java/com/example/secureweb/WebSecurityConfig.java
+++ src/main/java/com/example/secureweb/WebSecurityConfig.java
@@ -22,6 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
+                .defaultSuccessUrl("/home")
                 .permitAll()
                 .and()
                 .logout()
