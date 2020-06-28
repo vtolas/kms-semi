694e8d382626d65ab618fefa2e0fd9700bc996de
ben <ben.riegel@mni.thm.de>
2020-06-28 15:51:06 +0200

    Fahrer und fahrerabwicklung

---
+++ src/main/java/com/example/secureweb/config.java
@@ -0,0 +1,18 @@
+package com.example.secureweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class config implements WebMvcConfigurer {
+
+    // Configure app URLs
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        //registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
