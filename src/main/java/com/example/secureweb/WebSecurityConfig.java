2604057bcd82ba903d7ba86ea9194cf52fb66017
vtolas <vasileios.tolas@mni.thm.de>
2020-06-28 15:40:08 +0200

    permit only /

--- src/main/java/com/example/secureweb/WebSecurityConfig.java
+++ src/main/java/com/example/secureweb/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/profil", "/search-form", "/offer-form", "/result", "/reviews").permitAll()
+                .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
