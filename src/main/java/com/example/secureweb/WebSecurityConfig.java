584550b86da81f407545531dce1589bcb939cb66
Alaa El Bouhdidi <alaa.el.bouhdidi@mni.thm.de>
2020-07-03 01:47:44 +0200

    POST GET Fahrangebote in my-offers.html and offer-form.html

--- src/main/java/com/example/secureweb/WebSecurityConfig.java
+++ src/main/java/com/example/secureweb/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.secureweb;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,8 +17,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
---
+++ src/main/java/com/example/secureweb/service/FahrAngebotService.java
@@ -0,0 +1,32 @@
+package com.example.secureweb.service;
+
+import com.example.secureweb.entity.FahrAngebot;
+import com.example.secureweb.repository.FahrAngebotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FahrAngebotService {
+    @Autowired
+    private FahrAngebotRepository repo;
+
+    public FahrAngebot saveFahrAngebot(FahrAngebot fa){
+       return repo.save(fa);
+    }
+
+    public List<FahrAngebot> saveFahrAngebote(List<FahrAngebot> lfa){
+        return repo.saveAll(lfa);
+    }
+    public List<FahrAngebot> getFahrAngebote(){
+        return repo.findAll();
+    }
+    public FahrAngebot getFahrAngebotById(int id){
+        return repo.findById(id).orElse(null);
+    }
+    public String deleteFahrAngebot(int id){
+        repo.deleteById(id);
+        return "removed, id:" + id ;
+    }
+}
