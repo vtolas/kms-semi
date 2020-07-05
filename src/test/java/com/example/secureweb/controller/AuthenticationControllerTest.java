4661f58310fa8ac04f311c1be4c22132a0379e1c
vtolas <vasileios.tolas@mni.thm.de>
2020-07-05 20:45:50 +0200

    add empty tests

---
+++ src/test/java/com/example/secureweb/controller/AuthenticationControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.secureweb.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationControllerTest {
+
+}
---
+++ src/test/java/com/example/secureweb/entity/RoleTest.java
@@ -0,0 +1,7 @@
+package com.example.secureweb.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleTest {
+
+}
